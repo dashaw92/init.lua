c79f3a4595f45704a0ec61937f67a023725f64ed
dashaw92 <dashaw92@gmail.com>
2023-03-26 11:36:42 -0400

    No zig

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "rust", "java", "lua", "vim", "help", "zig" },
+  ensure_installed = { "rust", "java", "lua", "vim", "help" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
